ffaee4c57f2f53bd9c378b69a4f94e30b559b69f
Ergashev Ulug`bek <57306599+ErgashevUlugbek@users.noreply.github.com>
2020-03-22 19:05:35 +0500

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let zero = /\/0/g
+    expr = expr.replace(/\s+/g, '');
+    if(expr.includes('/0')) { throw new Error('TypeError: Division by zero.');}
+    //if(expr.match(/[^(]/g).length != expr.match(/[^)]/g)) { throw new Error('ExpressionError: Brackets must be paired') }
+    if ( expr.replace(/[^(]/g, "").length != expr.replace(/[^)]/g, "").length ) {
+        throw new Error("ExpressionError: Brackets must be paired");
+      }
+    return +new Function('return ' + expr)();
 }
 
 module.exports = {
